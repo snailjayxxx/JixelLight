ce022fe6f852a97fe0254fd9ede9980894898f0c
snailjayxxx <42602200+snailjayxxx@users.noreply.github.com>
2026-08-18 12:32:39 +0900

    Expose RAW working-space metadata

--- core/raw/RawDecoder.h
+++ core/raw/RawDecoder.h
@@ -9,6 +9,11 @@ struct RawMetadata {
     int width = 0;
     int height = 0;
     int bitsPerChannel = 0;
+    QString workingSpace = QStringLiteral("Linear ProPhoto RGB");
+    QString demosaic = QStringLiteral("AHD");
+    bool cameraMatrixEnabled = true;
+    bool cameraWhiteBalanceEnabled = true;
+    bool highlightBlendEnabled = true;
 };
 
 class RawDecoder final {
