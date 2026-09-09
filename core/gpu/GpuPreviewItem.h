3ecb8964af1697d4643d2477b92dd032e609347a
snailjayxxx <42602200+snailjayxxx@users.noreply.github.com>
2026-09-10 08:20:05 +0900

    alpha.11: carry monitor display LUT into GPU preview

--- core/gpu/GpuPreviewItem.h
+++ core/gpu/GpuPreviewItem.h
@@ -2,6 +2,8 @@
 #include <QQuickRhiItem>
 #include <QPointer>
 #include <QTimer>
+#include <QImage>
+#include <QString>
 class PhotoController;
 class GpuPreviewItem : public QQuickRhiItem {
     Q_OBJECT
@@ -11,10 +13,17 @@ public:
     QObject *controller() const;
     void setController(QObject *controller);
     QQuickRhiItemRenderer *createRenderer() override;
+    QImage displayColorLut() const { return m_displayColorLut; }
+    QString displayColorLutKey() const { return m_displayColorLutKey; }
+    QString displayColorProfileName() const { return m_displayColorProfileName; }
 signals:
     void controllerChanged();
 private:
     void checkBackend();
+    void refreshDisplayColorManagement();
     QPointer<PhotoController> m_controller;
     QTimer m_histogramRefresh;
+    QImage m_displayColorLut;
+    QString m_displayColorLutKey;
+    QString m_displayColorProfileName;
 };
